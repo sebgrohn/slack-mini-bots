5fc643456aa5171631d82c0c56b4046740acfba8
Ubuntu <ubuntu@sebastian-manual-machine-slack.localdomain>
2015-07-23 18:23:38 +0200

    Raise error if trying to use undefined Slack team, channel or webhook url.

--- slack_common.rb
+++ slack_common.rb
@@ -5,15 +5,27 @@ end
 
 def get_slack_team_config(team_arg)
   config = load_config()
-  config['teams'][team_arg || ''] or config['teams'][config['default_team']]
+  if team_arg.nil?
+    config['teams'][config['default_team']]
+  else
+    config['teams'][team_arg] or raise ArgumentError, "Unknown team"
+  end
 end
 
 def get_slack_channel(team_arg, channel_arg)
   config = get_slack_team_config(team_arg)
-  config['channels'][channel_arg || ''] or config['channels'][config['default_channel']]
+  if channel_arg.nil?
+    config['channels'][config['default_channel']]
+  else
+    config['channels'][channel_arg] or raise ArgumentError, "Unknown channel"
+  end
 end
 
 def get_slack_url(team_arg, url_arg)
   config = get_slack_team_config(team_arg)
-  config['urls'][url_arg || ''] or config['urls'][config['default_url']]
+  if url_arg.nil?
+    config['urls'][config['default_url']]
+  else
+    config['urls'][url_arg] or raise ArgumentError, "Unknown url"
+  end
 end
